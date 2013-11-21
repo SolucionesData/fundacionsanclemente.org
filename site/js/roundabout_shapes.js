2c9ab152252cccd3541f527cf0f612c151be0f9d
diegotorresamarilla <diegotorresamarilla@gmail.com>
2013-11-21 17:19:05 -0300

    Retoque del diseño

--- site/js/roundabout_shapes.js
+++ site/js/roundabout_shapes.js
@@ -39,6 +39,14 @@
  */
 jQuery.extend(jQuery.roundabout_shape,
 {
+	lazySusan: function (r, a, t) {
+		return {
+			x: Math.sin(r + a),
+			y: (Math.sin(r + 3 * Math.PI / 2 + a) / 8) * t,
+			z: (Math.cos(r + a) + 1) / 2,
+			scale: (Math.sin(r + Math.PI / 2 + a) / 2) + 0.5
+		};
+	},
 	theJuggler: function(r, a, t) {	
 		return {
 			x: Math.sin(r + a), 
